04a92892a31827e872821349521d123fd0dab550
sotaireange <sotaireange@gmail.com>
2025-08-29 00:37:51 +0600

    fix error

--- src/app/data/csv_handler.py
+++ src/app/data/csv_handler.py
@@ -140,9 +140,9 @@ class DataHandler:
                 if os.path.exists(filepath):
                     existing_data = pd.read_parquet(filepath, engine='pyarrow')
                     combined_data = pd.concat([existing_data, filtered_data], ignore_index=True)
-                    combined_data.to_parquet(filepath, engine='pyarrow',index=result.index)
+                    combined_data.to_parquet(filepath, engine='pyarrow',index=combined_data.index)
                 else:
-                    filtered_data.to_parquet(filepath, engine='pyarrow',index=result.index)
+                    filtered_data.to_parquet(filepath, engine='pyarrow',index=filtered_data.index)
 
     def save_analysis(self,df:pd.DataFrame,name:str,coin:bool=False):
         filepath=self._get_filepath_analysis(name,coin)
